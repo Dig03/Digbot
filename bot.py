eb278da443114f6991e7dd67bd09bb5671d0a6b0
Dig <Dig03@users.noreply.github.com>
2018-04-14 19:04:04 +0100

    Various improvements to code. In particular: more consistent string delimiter usage, cleaning and repositioning of imports, improvement of logging code, removing superfluous definitions from commands.Bot, changing the on_ready message, reworked error handling, addition of paging function and fixing a bug where the "define" command would fail due to messages being in excess of 2000 characters.

--- bot.py
+++ bot.py
@@ -1,50 +1,41 @@
 import discord
 from discord.ext import commands
 import asyncio
-import time
-import os
 import logging
+import time
 import random
+from os import getenv
 from wordnik import *
 from collections import OrderedDict
-from traceback import format_exc
-from sys import exc_info
 
-main_logger = logging.getLogger('main')
-main_logger.setLevel(logging.INFO)
+logger = logging.getLogger('main')
+logger.setLevel(logging.INFO)
 discord_logger = logging.getLogger('discord')
 discord_logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(name)s: %(message)s')
 
-main_console = logging.StreamHandler()
-main_console.setLevel(logging.INFO)
-main_console.setFormatter(formatter)
-main_logger.addHandler(main_console)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(formatter)
+logger.addHandler(console)
+discord_logger.addHandler(console)
 
-discord_console = logging.StreamHandler()
-discord_console.setLevel(logging.ERROR)
-discord_console.setFormatter(formatter)
-discord_logger.addHandler(discord_console)
 
-bot = commands.Bot(command_prefix='.', description='A shitty bot.', command_not_found='Command {} not found.')
+bot = commands.Bot(command_prefix='.', description="A shitty bot.")
 
 
 @bot.event
 async def on_ready():
-    main_logger.info('Logged in as {}, with ID {}'.format(bot.user.name, bot.user.id))
-    await bot.change_presence(game=discord.Game(name='Type {}help'.format(bot.command_prefix)))
+    logger.info("Ready! Username: {}, ID: {}.".format(bot.user.name, bot.user.id))
+    await bot.change_presence(game=discord.Game(name="Type {}help".format(bot.command_prefix)))
 
 
 @bot.event
-async def on_error(event, *args, **kwargs):
-    if exc_info()[0] is discord.errors.HTTPException:
-        await bot.say('Unable to process command, the response text is >2000 characters.')
-    else:
-        main_logger.error(format_exc())
-        if event == 'on_message':
-            message = args[0]
-            main_logger.error('Message content: ' + message.content)
-            await bot.say('An internal error occurred. This event has been automatically logged.')
+async def on_command_error(exception, context):
+    logger.error("Encountered exception: {}, as a result of message: '{}'".format(exception, context.message.content))
+
+
+# COMMANDS
 
 
 @bot.command()
@@ -56,14 +47,14 @@ async def echo(text):
 @bot.command()
 async def gettime():
     """Get the current local time of the bot."""
-    await bot.say(time.strftime('It is %a, %d %b %Y %H:%M:%S.', time.localtime()))
+    await bot.say(time.strftime("It is %a, %d %b %Y %H:%M:%S", time.localtime()))
 
 
 @bot.command()
 async def roulette():
     """Play Russian Roulette."""
-    if random.random() <= 0.1666:
-        await bot.say('''```
+    if random.random() <= 1/6:
+        await bot.say("""```
 BBBBBBBBBBBBBBBBB               AAA               NNNNNNNN        NNNNNNNN        GGGGGGGGGGGGG
 B::::::::::::::::B             A:::A              N:::::::N       N::::::N     GGG::::::::::::G
 B::::::BBBBBB:::::B           A:::::A             N::::::::N      N::::::N   GG:::::::::::::::G
@@ -79,24 +70,22 @@ BB:::::B     B:::::B         A:::::::A            N:::::::::N     N::::::N  G:::
 BB:::::BBBBBB::::::BA:::::A             A:::::A   N::::::N      N::::::::N  G:::::GGGGGGGG::::G
 B:::::::::::::::::BA:::::A               A:::::A  N::::::N       N:::::::N   GG:::::::::::::::G
 B::::::::::::::::BA:::::A                 A:::::A N::::::N        N::::::N     GGG::::::GGG:::G
-BBBBBBBBBBBBBBBBBAAAAAAA                   AAAAAAANNNNNNNN         NNNNNNN        GGGGGG   GGGG```''')
+BBBBBBBBBBBBBBBBBAAAAAAA                   AAAAAAANNNNNNNN         NNNNNNN        GGGGGG   GGGG```""")
     else:
-        await bot.say('click')
+        await bot.say("click")
 
 
 @bot.command()
 async def roll(dice):
-    """Roll an NdN dice."""
+    """Roll NdN di(c)e."""
     try:
         count, sides = map(int, dice.split('d'))
     except ValueError:
-        await bot.say('Format must be NdN.')
+        await bot.say("Format must be NdN.")
         return
-
     if count > 100 or sides > 1000:
-        await bot.say('Too many dice or sides.')
+        await bot.say("Too many dice or sides.")
         return
-
     result = ', '.join(str(random.randint(1, sides)) for _ in range(count))
     await bot.say(result)
 
@@ -107,6 +96,10 @@ def uniques(seq):
     return [x for x in seq if not (x in seen or seen_add(x))]
 
 
+def page(string, length):
+    return [string[start:start + length] for start in range(0, len(string), length)]
+
+
 @bot.command()
 async def define(word):
     """Get dictionary definitions of words."""
@@ -115,7 +108,7 @@ async def define(word):
     if definitions is None:
         definitions = wordApi.getDefinitions(word, sourceDictionaries='ahd')
     if definitions is None:
-        await bot.say('No definition found.')
+        await bot.say("No definition found.")
     else:
         parts_of_speech = uniques([d.partOfSpeech for d in definitions])
         parsed_defs = OrderedDict()
@@ -130,12 +123,17 @@ async def define(word):
             for text in parsed_defs[part_of_speech]:
                 definition_string += '\t{}. {}\n'.format(pos, text)
                 pos += 1
-        await bot.say('```' + definition_string + '```')
+        if len(definition_string) + 6 > 2000:
+            paged = page(definition_string, 1994)
+            for string in paged:
+                await bot.say("```{}```".format(string))
+        else:
+            await bot.say("```{}```".format(definition_string))
 
 
-tokens = {'discord': os.getenv('discord'), 'wordnik': os.getenv('wordnik')}
+tokens = {'discord': getenv('discord'), 'wordnik': getenv('wordnik')}
 if None in tokens.values():
-    raise EnvironmentError('tokens missing, cannot launch')
+    raise EnvironmentError('Tokens missing, cannot launch.')
 
 wordApi = WordApi.WordApi(swagger.ApiClient(tokens['wordnik'], 'http://api.wordnik.com/v4'))
 bot.run(tokens['discord'])
