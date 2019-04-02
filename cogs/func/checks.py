4c69d7f337ae1b4cce4f28778f5182318112f595
Dig <Dig03@users.noreply.github.com>
2019-04-02 10:35:02 +0100

    Fix owner check since IDs are now integers.

--- cogs/func/checks.py
+++ cogs/func/checks.py
@@ -3,5 +3,5 @@ from discord.ext import commands
 
 def is_owner():
     def p(ctx):
-        return ctx.message.author.id == '196391063987027969'
+        return ctx.message.author.id == 196391063987027969
     return commands.check(p)
