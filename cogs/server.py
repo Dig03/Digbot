4f584ba2f58bb48edce582145b4c48b12514ab56
Dig <Dig03@users.noreply.github.com>
2019-11-17 16:02:37 +0000

    Implemented clean command.

--- cogs/server.py
+++ cogs/server.py
@@ -2,12 +2,19 @@ from discord.ext import commands
 
 
 class Server(commands.Cog, name="Server"):
+    """Utility commands specifically relating to server functions."""
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def test(self, ctx):
-        await ctx.send("Hello, world!")
+    @commands.has_permissions(administrator=True)
+    async def clean(self, ctx, count: int):
+        """Delete <count> messages."""
+        deleted = await ctx.channel.purge(limit=count, before=ctx.message)
+        msg = await ctx.send("Successfully deleted {} message{}.".format(len(deleted),
+                                                                         's' if len(deleted) > 1 else ''))
+        await msg.delete(delay=2)
 
 
 def setup(bot):
